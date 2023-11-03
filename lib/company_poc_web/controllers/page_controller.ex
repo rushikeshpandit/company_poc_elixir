bb7fbd23f2fd6ab31486962602a485a4f80099d0
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2023-11-03 19:32:45 +0530

    - Added logic to redirect user to login OR company page depending upon login state

--- lib/company_poc_web/controllers/page_controller.ex
+++ lib/company_poc_web/controllers/page_controller.ex
@@ -4,6 +4,10 @@ defmodule CompanyPocWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    if conn.assigns[:current_user] do
+      redirect(conn, to: "/company")
+    else
+      redirect(conn, to: "/users/log_in")
+    end
   end
 end
