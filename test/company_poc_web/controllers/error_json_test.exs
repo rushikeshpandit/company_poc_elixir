feb4e25b5c1cb99e7cd18560b0c512cce54f248d
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2023-11-03 17:28:08 +0530

    - Re-initialise project

---
+++ test/company_poc_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule CompanyPocWeb.ErrorJSONTest do
+  use CompanyPocWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert CompanyPocWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert CompanyPocWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
