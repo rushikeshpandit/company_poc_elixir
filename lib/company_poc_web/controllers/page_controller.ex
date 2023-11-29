131f4470339def0183bf5c09ab21a55bf5d70748
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2023-11-29 15:46:59 +0530

    - Navigating user to login page by default

--- lib/company_poc_web/controllers/page_controller.ex
+++ lib/company_poc_web/controllers/page_controller.ex
@@ -4,6 +4,6 @@ defmodule CompanyPocWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    redirect(conn, to: "/users/log_in")
   end
 end
